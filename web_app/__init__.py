b6e795971f44f7c55b44098cc418ef8ef4c3e17f
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-21 13:15:05 -0400

    Config db via env var

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,5 +1,7 @@
 # web_app/__init__.py
 
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
 from web_app.models import db, migrate
@@ -8,17 +10,16 @@ from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
-#DATABASE_URI = "sqlite:///web_app_99.db" # using relative filepath
-DATABASE_URI = "sqlite:////Users/mjr/Desktop/twitoff-14/twitoff_development_14.db" # using absolute filepath on Mac (recommended)
-#DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+load_dotenv()
 
-SECRET_KEY = "super secret" # todo: use env var to customize
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = SECRET_KEY
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
